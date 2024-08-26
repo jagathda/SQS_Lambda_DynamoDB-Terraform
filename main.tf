7e495d1ac2cfc335747b531b625630afbac0c03d
jagathda <mail.jagath@gmail.com>
2024-08-26 22:17:25 +0300

    Added DynamoDB table

--- main.tf
+++ main.tf
@@ -30,3 +30,19 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+//DynamoDB table
+resource "aws_dynamodb_table" "my_table" {
+  name           = "my-table"
+  billing_mode    = "PAY_PER_REQUEST"
+  hash_key        = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "my-table"
+  }
+}
+
