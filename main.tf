c3255ce7153dc1ca469830b19b8ee50e36389116
jagathda <mail.jagath@gmail.com>
2024-08-26 22:11:19 +0300

    Added IAM role

--- main.tf
+++ main.tf
@@ -12,3 +12,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+//IAM role
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
